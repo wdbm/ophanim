70fcd0354c0d96635462f2b9cddf74d87167d0c8
Will Breaden Madden <wbm@protonmail.ch>
2017-02-22 23:28:31 +0000

    ophanim

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # ophanim
+
+![](ophanim.png)
+
 monitoring and notifications for measurements such as Bitcoin value and internet connection security
+
+# introduction
+
+Ophanim is a monitoring program that is designed to be run in the background. It monitors measurements like Bitcoin price fluctuations and internet connection security and sends alerts about these things in the form of desktop notifications. Under consideration are Telegram report messages.
+
+![](notifications.gif)
+
+# setup
+
+```Bash
+sudo pip install ophanim
+```
+
+# usage
+
+```Bash
+ophanim.py --help
+```
+
+```Bash
+ophanim.py
+```
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "python_ophanim",
+        version          = "2017.02.22.2326",
+        description      = "monitoring and notifications for measurements such as Bitcoin value and internet connection security",
+        long_description = long_description(),
+        url              = "https://github.com/wdbm/ophanim",
+        author           = "Will Breaden Madden",
+        author_email     = "wbm@protonmail.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "ophanim"
+                           ],
+        install_requires = [
+                           "denarius",
+                           "propyte",
+                           "shijian"
+                           ],
+        scripts          = [
+                           "ophanim.py"
+                           ],
+        entry_points     = """
+            [console_scripts]
+            ophanim = ophanim:ophanim
+        """
+    )
+
+def long_description(
+    filename = "README.md"
+    ):
+
+    if os.path.isfile(os.path.expandvars(filename)):
+        try:
+            import pypandoc
+            long_description = pypandoc.convert_file(filename, "rst")
+        except ImportError:
+            long_description = open(filename).read()
+    else:
+        long_description = ""
+    return long_description
+
+if __name__ == "__main__":
+    main()
